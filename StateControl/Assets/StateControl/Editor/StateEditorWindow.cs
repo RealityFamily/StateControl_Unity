bcc98eb64dc946f1ad245a05001f23e3bdf3bed0
Leo <leonis13579@gmail.com>
2020-06-24 13:13:03 +0300

    Created top menu
    Created top menu with:
    - Open StateEditor;
    - Creating State list object on scene

---
+++ StateControl/Assets/StateControl/Editor/StateEditorWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+public class StateEditorWindow : EditorWindow
+{
+    private GameObject _states = null;
+
+    private void OnGUI()
+    {
+        _states = GameObject.FindGameObjectWithTag("States");
+
+        if (_states != null)
+        {
+            DrawStateEditor();
+        } else
+        {
+            DrawStateListError();
+        }
+    }
+
+    private void DrawStateListError()
+    {
+        GUILayout.Label("Add States from the top menu to change it.", EditorStyles.boldLabel);
+    }
+
+    private void DrawStateEditor()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+#endif
---
+++ StateControl/Assets/StateControl/Scripts/WebConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using WebSocketSharp;
+using UnityEngine;
+
+public class WebConnection : MonoBehaviour
+{
+    public string BaseURL;
+
+    private States states = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        states = gameObject.GetComponent<States>();
+
+        if (!string.IsNullOrWhiteSpace(BaseURL)) {
+            using (WebSocket ws = new WebSocket(BaseURL))
+            {
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+
+                ws.Send(JsonUtility.ToJson(states));
+            }
+        }
+    }
+
+    private void Ws_OnMessage(object sender, MessageEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnOpenHandler(object sender, EventArgs e)
+    {
+        Debug.Log("WebSocket connected!");
+    }
+}
