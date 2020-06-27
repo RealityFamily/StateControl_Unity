1b9d7b59a24a86f41a63ad65017086588c906573
Leo <leonis13579@gmail.com>
2020-06-27 20:12:45 +0300

    Written README.md file

---
+++ StateControl/Editor/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Buttons
+{
+    public static bool Delete()
+    {
+        return GUILayout.Button(EditorGUIUtility.IconContent("d_TreeEditor.Trash"), GUILayout.Width(30));
+    }
+
+    public static bool Add()
+    {
+        return GUILayout.Button("+");
+    }
+
+    public static bool PackAndSend()
+    {
+        return GUILayout.Button("Pack and Send");
+    }
+}
